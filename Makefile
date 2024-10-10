b480c211f4582192f6e2ca9c8fe37e63a161d1ef
alongnice <48875845+alongnice@users.noreply.github.com>
2024-10-10 02:15:35 +0000

    优化：同步webhook使用同样的http对象构建策略

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 .PHONY: build clean test test-race
 # 支持参数
 
-VERSION=3.3.3
+VERSION=3.3.4
 BIN=myddns
 DIR_SRC=.
 DOCKER_CMD=docker
--- config/webhook.go
+++ config/webhook.go
@@ -8,7 +8,6 @@ import (
 	"net/http"
 	"net/url"
 	"strings"
-	"time"
 )
 
 type Webhook struct {
@@ -101,8 +100,7 @@ func ExecWebhook(domains *Domains, conf *Config) {
 
 		req.Header.Add("content-type", contentType)
 
-		clt := http.Client{}
-		clt.Timeout = 30 * time.Second
+		clt := util.CreateHTTPClient()
 		resp, err := clt.Do(req)
 		body, err := util.GetHTTPResponseOrg(resp, requestURL, err)
 		if err == nil {
